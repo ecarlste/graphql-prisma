f048e2c5e3ab71f19a15bc4efea0c97c11d6f2cd
Erik S. Carlsten <ecarlste@gmail.com>
2019-04-01 14:40:07 -0600

    Added myPost subscription

--- src/resolvers/Subscription.resolver.js
+++ src/resolvers/Subscription.resolver.js
@@ -1,3 +1,5 @@
+import getUserId from '../utils/getUserId';
+
 export default {
   post: {
     subscribe(_, _1, { prisma }, info) {
@@ -7,6 +9,24 @@ export default {
   comment: {
     subscribe(_, { postId }, { prisma }, info) {
       return prisma.subscription.comment({ where: { node: { post: { id: postId } } }, info });
+    }
+  },
+  myPost: {
+    subscribe(_, _1, { prisma, request }, info) {
+      const userId = getUserId(request);
+
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              author: {
+                id: userId
+              }
+            }
+          }
+        },
+        info
+      );
     }
   }
 };
