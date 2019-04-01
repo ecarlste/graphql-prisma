38582b37ba931d9be64ef3efebc15c7e946a01c1
Erik S. Carlsten <ecarlste@gmail.com>
2019-04-01 10:03:30 -0600

    User resolver only returns logged in users email

--- src/resolvers/User.resolver.js
+++ src/resolvers/User.resolver.js
@@ -1,3 +1,15 @@
-const User = {};
+import getUserId from '../utils/getUserId';
+
+const User = {
+  email(parent, args, { request }) {
+    const userId = getUserId(request, false);
+
+    if (userId && userId === parent.id) {
+      return parent.email;
+    }
+
+    return null;
+  }
+};
 
 export default User;
