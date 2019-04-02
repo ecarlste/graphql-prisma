8029c0a35744c1ae26851c8929ac51254d762c8d
Erik S. Carlsten <ecarlste@gmail.com>
2019-04-02 10:44:18 -0600

    Added pagination to users and posts queries

--- src/resolvers/Query.resolver.js
+++ src/resolvers/Query.resolver.js
@@ -2,7 +2,10 @@ import getUserId from '../utils/getUserId';
 
 export default {
   users(_, args, { prisma }, info) {
-    const opArgs = {};
+    const opArgs = {
+      first: args.first,
+      skip: args.skip
+    };
 
     if (args.query) {
       opArgs.where = {
@@ -20,7 +23,9 @@ export default {
     const opArgs = {
       where: {
         published: true
-      }
+      },
+      first: args.first,
+      skip: args.skip
     };
 
     if (args.query) {
